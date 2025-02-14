700b88b64e5cf5b0b03bda0e7d21311f89faa138
Yan Lanna Alexandre <yanlanna2@hotmail.com>
2025-02-14 18:03:40 +0100

    Using FSST library works!

--- main.cpp
+++ main.cpp
@@ -7,12 +7,16 @@ using namespace duckdb;
 int main() {
 	DuckDB db(nullptr);
 	Connection con(db);
-	const auto result = con.Query("SELECT \"Case Number\" FROM read_csv('/Users/yanlannaalexandre/_DA_REPOS/fsst-plus-experiments/example_data/Chicago Crimes 2012-2017.csv', strict_mode=False);");
+	const auto result = con.Query("SELECT \"Case Number\" FROM read_csv('/Users/yanlannaalexandre/_DA_REPOS/fsst-plus-experiments/example_data/Chicago Crimes 2012-2017.csv', strict_mode=False) LIMIT 10000;");
 
+	// Store original strings for verification
+	std::vector<std::string> original_strings;
+	original_strings.reserve(10000);
 	// Vectors to store string lengths and pointers
 	std::vector<size_t> lenIn;
+	lenIn.reserve(10000);
 	std::vector<const unsigned char*> strIn;
-
+	strIn.reserve(10000);
 	auto next_chunk = result->Fetch();
 	while (next_chunk) {
 		// to get the number of values within the chunk, we can use the size() method of the chunk
@@ -23,10 +27,17 @@ int main() {
 
 
 		// Populate lenIn and strIn
-		for (size_t i = 0; i < chunk_size; i++) {
-			string_t str = vector_data[i];
-			lenIn.push_back(str.GetSize());
-			strIn.push_back(reinterpret_cast<const unsigned char*>(str.GetDataUnsafe()));
+		for (size_t i = 0; i < next_chunk->size(); i++) {
+			if (!vector_data[i].Empty()) {
+				std::string str = vector_data[i].GetString();
+
+				original_strings.push_back(str);
+				const std::string &stored_str = original_strings.back();
+				lenIn.push_back(stored_str.size());
+				// strIn.push_back(reinterpret_cast<const unsigned char*>(str.c_str())); // This is not working, it just points to the single str value
+				//TODO: Fix this. Make it so it's an array of pointers to each string in original_strings
+				strIn.push_back(reinterpret_cast<const unsigned char*>(stored_str.c_str()));
+			}
 		}
 		break;
 		// get the next chunk, start loop again
@@ -34,18 +45,87 @@ int main() {
 	}
 
 	// Create FSST encoder
-	int zeroTerminated = 0; // DuckDB strings are not zero-terminated
+	const int zeroTerminated = 0; // DuckDB strings are not zero-terminated
 	fsst_encoder_t* encoder = fsst_create(
-		lenIn.size(),        // Number of strings
-		lenIn.data(),        // Array of string lengths
-		strIn.data(),        // Array of string pointers
-		zeroTerminated       // Not zero-terminated
+		2000,        /* IN: number of strings in batch to sample from. */
+		lenIn.data(),        /* IN: byte-lengths of the inputs */
+		strIn.data(),        /* IN: string start pointers. */
+		zeroTerminated       /* IN: whether input strings are zero-terminated. If so, encoded strings are as well (i.e. symbol[0]=""). */
 	);
 
-	// (Optional: Use encoder for compression here)
-	std::cout << "I am alive";
+	// Prepare compression outputs
+	std::vector<size_t> lenOut(lenIn.size());
+	std::vector<unsigned char*> strOut(lenIn.size());
+
+	// Calculate worst-case output size (2*input)
+	size_t max_out_size = 0;
+	for (auto len : lenIn) max_out_size += 2 * len;
+
+	// Allocate output buffer
+	unsigned char* output = static_cast<unsigned char*>(malloc(max_out_size));
+
+	// Perform compression
+	size_t num_compressed = fsst_compress(
+		encoder,				/* IN: encoder obtained from fsst_create(). */
+	lenIn.size(),/* IN: number of strings in batch to compress. */
+		lenIn.data(),/* IN: byte-lengths of the inputs */
+		strIn.data(), /* IN: input string start pointers. */
+		max_out_size,/* IN: byte-length of output buffer. */
+		output,/* OUT: memory buffer to put the compressed strings in (one after the other). */
+		lenOut.data(),/* OUT: byte-lengths of the compressed strings. */
+		strOut.data()/* OUT: output string start pointers. Will all point into [output,output+size). */
+	);
+
+	// Print compressed strings
+	for (size_t i = 0; i < num_compressed; i++) {
+		std::cout << "Compressed string :" << strIn[i]
+				  << " to ";
+
+		for (size_t j = 0; j < lenOut[i]; j++) {
+			std::cout << static_cast<int>(strOut[i][j]) << " "; // Print each byte as an integer
+		}
+		std::cout << std::endl;
+	}
+
+
+	// Get decoder for decompression
+	fsst_decoder_t decoder = fsst_decoder(encoder);
+
+	// Verify compression correctness
+	size_t total_original = 0, total_compressed = 0;
+	for (size_t i = 0; i < num_compressed; i++) {
+		// Allocate decompression buffer
+		auto* decompressed = static_cast<unsigned char*>(malloc(lenIn[i]));
+
+		size_t decompressed_size = fsst_decompress(
+			&decoder,
+			lenOut[i],
+			strOut[i],
+			lenIn[i],
+			decompressed
+		);
+		std::string_view decompressed_view(reinterpret_cast<char*>(decompressed), decompressed_size);
+		// Verify decompression
+		if (decompressed_size != lenIn[i] ||
+			decompressed_view != original_strings[i]) {
+			std::cerr << "Decompression mismatch for string " << i <<" Expected: "<< original_strings[i] <<" Got: " << decompressed_view <<std::endl;
+			}
+
+		total_original += lenIn[i];
+		total_compressed += lenOut[i];
+	}
+
+	// Print compression stats
+	std::cout << "Compressed " << num_compressed << " strings\n";
+	std::cout << "Original size: " << total_original << " bytes\n";
+	std::cout << "Compressed size: " << total_compressed << " bytes\n";
+	std::cout << "Compression ratio: "
+			  << (double)total_compressed/total_original * 100
+			  << "%\n";
+
 
 	// Cleanup
+	std::cout << "Cleanup";
 	fsst_destroy(encoder);
 
 
