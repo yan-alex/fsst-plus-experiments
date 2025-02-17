95bd633eaf17b9778cb435a1b84969c898136348
Yan Lanna Alexandre <yanlanna2@hotmail.com>
2025-02-17 09:35:05 +0100

    .

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace duckdb;
 int main() {
 	DuckDB db(nullptr);
 	Connection con(db);
-	const auto result = con.Query("SELECT \"Case Number\" FROM read_csv('/Users/yanlannaalexandre/_DA_REPOS/fsst-plus-experiments/example_data/Chicago Crimes 2012-2017.csv', strict_mode=False) LIMIT 10000;");
+	const auto result = con.Query("SELECT \"Case Number\" FROM read_csv('/Users/yanlannaalexandre/_DA_REPOS/fsst-plus-experiments/example_data/Chicago Crimes 2012-2017.csv', strict_mode=False) LIMIT 10;");
 
 	// Store original strings for verification
 	std::vector<std::string> original_strings;
@@ -84,7 +84,7 @@ int main() {
 		for (size_t j = 0; j < lenOut[i]; j++) {
 			std::cout << static_cast<int>(strOut[i][j]) << " "; // Print each byte as an integer
 		}
-		std::cout << std::endl;
+		std::cout << "\n";
 	}
 
 
@@ -98,17 +98,19 @@ int main() {
 		auto* decompressed = static_cast<unsigned char*>(malloc(lenIn[i]));
 
 		size_t decompressed_size = fsst_decompress(
-			&decoder,
-			lenOut[i],
-			strOut[i],
-			lenIn[i],
-			decompressed
+			&decoder, /* IN: use this symbol table for compression. */
+			lenOut[i],  /* IN: byte-length of compressed string. */
+			strOut[i], /* IN: compressed string. */
+			lenIn[i], /* IN: byte-length of output buffer. */
+			decompressed /* OUT: memory buffer to put the decompressed string in. */
 		);
-		std::string_view decompressed_view(reinterpret_cast<char*>(decompressed), decompressed_size);
+		const std::string_view decompressed_view(reinterpret_cast<char*>(decompressed), decompressed_size);
 		// Verify decompression
 		if (decompressed_size != lenIn[i] ||
 			decompressed_view != original_strings[i]) {
 			std::cerr << "Decompression mismatch for string " << i <<" Expected: "<< original_strings[i] <<" Got: " << decompressed_view <<std::endl;
+			} else {
+				std::cout << "Decompression successful for string " << i <<" Expected: "<< original_strings[i] <<" Got: " << decompressed_view <<std::endl;
 			}
 
 		total_original += lenIn[i];
@@ -123,10 +125,7 @@ int main() {
 			  << (double)total_compressed/total_original * 100
 			  << "%\n";
 
-
 	// Cleanup
 	std::cout << "Cleanup";
 	fsst_destroy(encoder);
-
-
 }
