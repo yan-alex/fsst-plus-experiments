834fb20085f33c2f5da4e1677589ad48f84d1e0c
Yan Alexandre <yanlanna2@hotmail.com>
2025-04-25 16:13:07 +0200

    Augment max prefix size to 255, as it's already compressed so no dreadful compression worries :)

--- src/config.h
+++ src/config.h
@@ -10,7 +10,7 @@ namespace config {
     extern const bool print_split_points; // whether to print split points.
     extern const bool print_decompressed_corpus;
 
-    constexpr size_t max_prefix_size = 120; // how far into the string to scan for a prefix. (max prefix size)
+    constexpr size_t max_prefix_size = 255; // how far into the string to scan for a prefix. (max prefix size)
     constexpr size_t amount_strings_per_symbol_table = 120000; // 120000 = a duckdb row group
     size_t global_index = 0;
 }
