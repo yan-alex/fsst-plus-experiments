4bc2518884b98348d17b1db041177b32c26d275c
Yan Alex <yanlanna2@hotmail.com>
2025-05-06 13:22:24 +0200

    .

--- src/env.h
+++ src/env.h
@@ -3,6 +3,7 @@
 #include <string>
 
 namespace env {
-    // const std::string project_dir = "/export/scratch2/home/yla/fsst-plus-experiments";
-    const std::string project_dir = "/Users/yanlannaalexandre/_DA_REPOS/fsst-plus-experiments";
+    const std::string project_dir = "/export/scratch2/home/yla/fsst-plus-experiments";
+    // const std::string project_dir = "/Users/yanlannaalexandre/_DA_REPOS/fsst-plus-experiments";
+    
 }
